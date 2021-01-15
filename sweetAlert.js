da235c5e79804ef8a00f2e3ccd6d48ca62f84a90
paolajose <paoj398@gmail.com>
2021-01-15 16:18:31 -0400

    primer intento de alerta

---
+++ sweetAlert.js
@@ -0,0 +1,7 @@
+swal.fire({
+    title:"Bienvenido",
+    text: "Por favor llene todos los campos",
+    icon: "info",
+    footer: '<span class="rojo"> Esta informacion es confidencial</span>',
+   
+})
